fe59a282c42c93e2e2440ce554852ab3da011abd
hannody <aljaboori@gmail.com>
2021-09-14 12:37:52 +0800

    Changing permission from AllowAny to IsAuthenticated, and updated the tests to examine both cases for authenticated and unauthenticated CRUD operations.

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -153,7 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated', ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
--- backend/core/puppies/test_views.py
+++ backend/core/puppies/test_views.py
@@ -1,5 +1,6 @@
 from random import randrange
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -16,7 +17,7 @@ class PuppiesViewsTest(TestCase):
     post_endpoint = '/api/v1/puppy/new/'
     delete_endpoint = '/api/v1/puppy/delete/'
     update_endpoint = '/api/v1/puppy/update/'
-    id = 1
+    ID = 1
 
     def setUp(self):
         """
@@ -30,13 +31,23 @@ class PuppiesViewsTest(TestCase):
 
         Puppy.objects.create(name='Ricky', age=6, breed='Labrador', color='Brown')
 
+        User = get_user_model()
+
+        self.user = User.objects.create_user(email='normal@user.com', password='foo')
+
     def get_random_id(self):
         """
-        Generate an integer value between two and the number (objects count) of Puppy model object plus 1.
+        Generate an integer value between number "2" and the count of (objects count) of Puppy model object plus 1.
         :return: an integer between 'id+1' and 'Puppy.objects.all().count()) + 1'
         """
         stop = Puppy.objects.all().count()
-        return randrange(self.id + 1, stop, 1)
+        random_id = randrange(start=self.ID, stop=stop, step=1)
+        return self.ID + random_id
+
+    def test_get_random_number(self):
+        stop = Puppy.objects.all().count()
+        random_id = randrange(self.ID, stop, 1)
+        self.assertTrue(random_id <= stop)
 
     def test_puppies_listview(self):
         """
@@ -45,11 +56,17 @@ class PuppiesViewsTest(TestCase):
         """
         self.get_random_id()
 
+        # Unauthenticated user action.
+        response = self.client.get(path=self.list_endpoint)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Authenticated user action.
+        self.client.login(email='normal@user.com', password='foo')
         response = self.client.get(path=self.list_endpoint)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertContains(response, Puppy.objects.get(id=self.id).name)
+        self.assertContains(response, Puppy.objects.get(id=self.ID).name)
 
         self.assertContains(response, Puppy.objects.get(id=self.get_random_id()).name)
 
@@ -58,11 +75,18 @@ class PuppiesViewsTest(TestCase):
         a test for getting all puppies without pagination, using the view 'name'.
         :return: Assertion
         """
+
+        # Unauthenticated user action.
+        response = self.client.get(reverse('puppies_list'))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Authenticated user action.
+        self.client.login(email='normal@user.com', password='foo')
         response = self.client.get(reverse('puppies_list'))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertContains(response, Puppy.objects.get(id=self.id).name)
+        self.assertContains(response, Puppy.objects.get(id=self.ID).name)
 
         self.assertContains(response, Puppy.objects.get(id=self.get_random_id()).name)
 
@@ -72,22 +96,21 @@ class PuppiesViewsTest(TestCase):
         :return: Assertion
         """
 
-        puppy = Puppy.objects.get(id=self.id)
-
+        puppy = Puppy.objects.get(id=self.ID)
         url = '{}{}/'.format(self.detail_endpoint, puppy.pk)
 
+        # Unauthenticated user action.
         response = self.client.get(path=url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+        # Authenticated user action.
+        self.client.login(email='normal@user.com', password='foo')
+        response = self.client.get(path=url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
         self.assertContains(response, puppy.name)
-
         self.assertContains(response, puppy.color)
-
         self.assertContains(response, puppy.age)
-
         self.assertContains(response, puppy.breed)
-
         self.assertNotContains(response, Puppy.objects.get(id=self.get_random_id()).name)
 
     def test_create_new_puppy(self):
@@ -102,6 +125,13 @@ class PuppiesViewsTest(TestCase):
             'color': 'Black and Orange'
         }
         previous_object_count = Puppy.objects.all().count()
+
+        # Unauthenticated user action.
+        response = self.client.post(path=self.post_endpoint, data=new_puppy, content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Authenticated user action.
+        self.client.login(email='normal@user.com', password='foo')
         response = self.client.post(path=self.post_endpoint, data=new_puppy, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Puppy.objects.count() > previous_object_count)
@@ -111,9 +141,16 @@ class PuppiesViewsTest(TestCase):
         Deleting puppy object CRU_Delete
         :return: Assertion
         """
-        puppy = Puppy.objects.get(id=self.id)
+        puppy = Puppy.objects.get(id=self.ID)
         previous_object_count = Puppy.objects.all().count()
         url = '{}{}/'.format(self.delete_endpoint, puppy.pk)
+
+        # Unauthenticated user action.
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Authenticated user action.
+        self.client.login(email='normal@user.com', password='foo')
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertTrue(Puppy.objects.count() < previous_object_count)
@@ -128,7 +165,14 @@ class PuppiesViewsTest(TestCase):
         old_name = puppy.name
         old_updated_at = puppy.updated_at
         updated_puppy_data = {"name": "Weasel", "age": 1, "breed": "Sloughi", "color": puppy.color}
+
+        # Unauthenticated user action.
         url = '{}{}/'.format(self.update_endpoint, puppy.pk)
+        response = self.client.put(path=url, data=updated_puppy_data, content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Authenticated user action.
+        self.client.login(email='normal@user.com', password='foo')
         response = self.client.put(path=url, data=updated_puppy_data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertNotContains(response, old_name)
@@ -147,7 +191,15 @@ class PuppiesViewsTest(TestCase):
         new_name = "Yalong"
         patch_puppy_data = {"name": new_name}
         url = '{}{}/'.format(self.update_endpoint, puppy.pk)
+
+        # Unauthenticated user action.
         response = self.client.patch(path=url, data=patch_puppy_data, content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Authenticated user action.
+        self.client.login(email='normal@user.com', password='foo')
+        response = self.client.patch(path=url, data=patch_puppy_data, content_type="application/json")
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertNotContains(response, old_name)
         self.assertContains(response, new_name)
