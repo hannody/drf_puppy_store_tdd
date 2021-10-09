eca7e23118df1f75821208ecf8dcc79290c31e60
Mohanad Abu-Nayla <aljaboori@gmail.com>
2021-10-09 10:47:07 +0800

    Updated: admin url has changed from default to custom one, relevant tests have been carried out and written.

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
+admin_url = 'd788b92cc1c9681bedee6a6fa9f67c33ae2f382251db8a620f83b09c50faad32/'
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html")),
     path('api/v1/', include('puppies.urls')),
     #path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
+    path(admin_url, admin.site.urls),
 ]
---
+++ users/test_admin.py
@@ -0,0 +1,31 @@
+from django.test import SimpleTestCase
+from rest_framework import status
+
+from backend.core.core.urls import admin_url
+
+
+class TestAdminCustomURL(SimpleTestCase):
+    """
+    Tests for Admin page reachability.
+    """
+
+    def setUp(self) -> None:
+        """
+        Init data for other tests.
+        """
+        self.custom_admin_url = '/{}{}/'.format(admin_url, 'login')
+        self.default_admin_ulr = '/{}/{}/'.format('admin', 'login')
+
+    def test_new_url(self):
+        """
+        Test for new admin url.
+        """
+        response = self.client.get(self.custom_admin_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_default_admin_url_404(self):
+        """
+        verifying the default 'admin/' url is no longer reachable.
+        """
+        response = self.client.get(self.default_admin_ulr)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
